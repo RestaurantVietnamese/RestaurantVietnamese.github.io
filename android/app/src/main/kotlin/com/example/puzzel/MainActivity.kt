4c05ffcff93d9d437d19ca089933351202ebdf79
hieupc <phanconghieu321@gmail.com>
2025-07-04 01:51:53 +0700

    Deploy web mới từ feature/code_game

--- android/app/src/main/kotlin/com/example/puzzel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.puzzel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "341046512"
+    serviceWorkerVersion: "2159452593"
   }
 });
