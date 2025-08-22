845f2ba90e23f7afa33ef2780891c19a43705823
hieupc <phanconghieu321@gmail.com>
2025-08-22 17:26:16 +0700

    update bash

--- deploy_web_2.sh
+++ deploy_web_2.sh
@@ -5,17 +5,19 @@ BUILD_PATH="/Users/pch/Desktop/personal/web_g18/build/web"
 TEMP_PATH="$HOME/web_temp"
 
 echo "🔄 Checkout code branch"
-git checkout feature/code_game || exit
+git checkout feature/code_game || exit 1
 
 echo "📁 Copying build from $BUILD_PATH to temp ($TEMP_PATH)"
+rm -rf "$TEMP_PATH"
 mkdir -p "$TEMP_PATH"
 cp -r "$BUILD_PATH"/* "$TEMP_PATH"/
 
 echo "🚀 Switching to main"
-git checkout main || exit
+git checkout main || exit 1
 
-echo "🧹 Cleaning main branch"
-find . -maxdepth 1 ! -name '.' ! -name '.git' -exec rm -rf {} +
+echo "🧹 Cleaning main branch (remove everything except .git)"
+shopt -s extglob
+rm -rf !(.git)
 
 echo "📥 Copying web build into main"
 cp -r "$TEMP_PATH"/* ./
