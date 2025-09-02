53590895d92967f78f5f9dcbd79a0840a6576cbd
hieupc <phanconghieu321@gmail.com>
2025-09-02 15:23:20 +0700

    Deploy web from feature/page_menu

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8b6cfc6ad61b4a905543473f5bb70fb6",
+const RESOURCES = {"flutter_bootstrap.js": "64081c923d9de22b26ef9dc8ff30fee2",
 "version.json": "8e940c198fa0e920c8dc64de4038025d",
 "index.html": "5bcab0416479b698e514b8ad76c64eef",
 "/": "5bcab0416479b698e514b8ad76c64eef",
-"main.dart.js": "5669f0abc30fb4ed4adaf841fbaedbe7",
+"main.dart.js": "bc87d437c131b1b5e810bce8505aac7f",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
