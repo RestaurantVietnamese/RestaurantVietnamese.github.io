4c801f63b28b22f94d01644feea727447ba32fa9
hieupc <phanconghieu321@gmail.com>
2025-09-13 01:44:13 +0700

    update

---
+++ deploy_web_4.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "🔄 Checkout code branch"
+git checkout feature/menu_page_2 || exit
+
+echo "🛠 Building web..."
+flutter build web || exit
+
+echo "📁 Copying build to temp"
+mkdir -p ~/web_temp
+cp -r build/web/* ~/web_temp/
+
+echo "🚀 Switching to main"
+git checkout main || exit
+
+echo "🧹 Cleaning main branch"
+find . -maxdepth 1 ! -name '.' ! -name '.git' -exec rm -rf {} +
+
+echo "📥 Copying web build into main"
+cp -r ~/web_temp/* ./
+
+echo "📦 Committing and pushing"
+git add .
+git commit -m "Deploy web from feature/page_menu"
+git push --force origin main
+git checkout feature/menu_page_2
+
+echo "✅ Done!"
