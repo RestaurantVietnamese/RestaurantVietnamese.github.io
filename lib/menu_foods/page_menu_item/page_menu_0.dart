d658b930368ef13bfd52e9779242bd805e32ac02
hieupc <phanconghieu321@gmail.com>
2026-02-13 06:55:02 +0700

    update

--- lib/menu_foods/page_menu_item/page_menu_0.dart
+++ lib/menu_foods/page_menu_item/page_menu_0.dart
@@ -24,91 +24,91 @@ class _PageMenu0State extends State<PageMenu0> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<FontCubit, FontState>(
-      builder: (context, state) => 
-      // InteractiveViewer(
-      //   boundaryMargin: EdgeInsets.all(100),
-      //   minScale: 0.1, // zoom nhỏ hơn để vừa khung
-      //   maxScale: 3.0,
-      //   child: 
-        FittedBox(
-          fit: BoxFit.contain, // scale toàn bộ nội dung vừa khung
-          alignment: Alignment.topLeft,
-          child: Stack(
-            children: [
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: SizedBox(
-                  height: 670,
-                  child: Image.asset(
-                    'assets/images/img_menu_page_0.jpg',
-                    fit: BoxFit.cover,
-                  ),
+      builder: (context, state) =>
+          // InteractiveViewer(
+          //   boundaryMargin: EdgeInsets.all(100),
+          //   minScale: 0.1, // zoom nhỏ hơn để vừa khung
+          //   maxScale: 3.0,
+          //   child:
+          FittedBox(
+        fit: BoxFit.contain, // scale toàn bộ nội dung vừa khung
+        alignment: Alignment.topLeft,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: SizedBox(
+                height: 670,
+                child: Image.asset(
+                  'assets/images/img_menu_page_0.jpg',
+                  fit: BoxFit.cover,
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                    padding: EdgeInsets.all(0),
-                    color: Colors.white,
-                    height: 130,
-                    child: QrCodeScreen()),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 130,
-                right: 0,
-                child: Container(
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                  padding: EdgeInsets.all(0),
+                  color: Colors.white,
                   height: 130,
-                  padding: EdgeInsets.only(left: 15, top: 5),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: const [
-                    Color(0xff2c4e33),
-                    Color(0xff426d46),
-                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 5),
-                        child: Text(
-                          'Sophienstraße 27-29, 76530 Baden-Baden',
-                          style: getTextStyleNormal(size: 13),
-                        ),
+                  child: QrCodeScreen()),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 130,
+              right: 0,
+              child: Container(
+                height: 130,
+                padding: EdgeInsets.only(left: 15, top: 5),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: const [
+                  Color(0xff2c4e33),
+                  Color(0xff426d46),
+                ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 5),
+                      child: Text(
+                        'Sophienstraße 27-29, 76530 Baden-Baden',
+                        style: getTextStyleNormal(size: 13),
                       ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Mo: Ruhetag',
-                        style: getTextStyleNormal(),
-                      ),
-                      Text(
-                        'So - Do: 11-22 Uhr | Fr - Sa: 11-23 Uhr Lieferservice | Takeaway',
-                        style: getTextStyleNormal(),
-                      ),
-                      Text(
-                        'Tel.: 07221 9706660',
-                        style: getTextStyleNormal(size: 26),
-                      ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Mo: Ruhetag',
+                      style: getTextStyleNormal(),
+                    ),
+                    Text(
+                      'So - Do: 11-22 Uhr | Fr - Sa: 11-23 Uhr Lieferservice | Takeaway',
+                      style: getTextStyleNormal(),
+                    ),
+                    Text(
+                      'Tel.: 07221 9706660',
+                      style: getTextStyleNormal(size: 26),
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                width: 450,
-                height: 800,
-                decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    border: Border.all(color: Colors.grey, width: 0.1)),
-                padding: EdgeInsets.only(left: 20, right: 20),
-              ),
-            ],
-          ),
+            ),
+            Container(
+              width: 450,
+              height: 800,
+              decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.grey, width: 0.1)),
+              padding: EdgeInsets.only(left: 20, right: 20),
+            ),
+          ],
         ),
+      ),
       // ),
     );
   }
